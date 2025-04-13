52c4435b576bbec71383421d4b385f61e0610a5e
oshada64322 <botoshada260@gmail.com>
2025-04-13 21:49:11 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~qZVxALJa#ZWrmqNyelULzYziEr-d-aBV_4Gd0FGZICPrfzx_YvO8",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~qB1FSIha#3Chi3IyQF7S2P4gzIWH0GMxdcH5bM-2Xu0_4xsPrzu0",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000",
 FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. oshada\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
