eef462825d727162674ca7b3094a0ced273cee6e
oshada64322 <botoshada260@gmail.com>
2025-04-13 21:49:54 +0530

    Add files via upload

--- session.js
+++ session.js
@@ -1,4 +1,4 @@
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+﻿//══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //                                                                                                      //
 //                                   𝗤𝗨𝗘𝗘𝗡-𝗔𝗡𝗝𝗨 𝗫ᴾᴿᴼ 𝐁𝐎𝐓                                               //
 //                                                                                                      //
@@ -40,8 +40,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~qB1FSIha#3Chi3IyQF7S2P4gzIWH0GMxdcH5bM-2Xu0_4xsPrzu0",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
-PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. oshada\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
-};
+    SESSION_ID: process.env.SESSION_ID || "your session id",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    PORT: process.env.PORT || "8000",
+    SESSION_DIR: process.env.SESSION_DIR || "session",
+    FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
+  };
+  
